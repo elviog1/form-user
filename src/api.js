4dbbf4697bbd82fe4a3af3c556dad9a7c93942b2
Elvio Galeano <elvio.galeano827@gmail.com>
2023-01-08 21:03:17 -0300

    fix

--- src/api.js
+++ src/api.js
@@ -1,9 +1,11 @@
 import {  addDoc, collection, getDocs, query } from 'firebase/firestore'
 import  {db} from './firebase'
 
+//Almacena un usuario a firebase
 export const saveItems = (data)=>{
     addDoc(collection(db,"user"),{data})
 }
+//Obtengo todos los usuarios de firebase
 export const getItemsFirebase = async ()=>{
     const result = await getDocs(query(collection(db,"user")))
     return result
